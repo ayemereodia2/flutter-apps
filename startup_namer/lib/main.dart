2d3328ffa9e9788bc8cb7c7e33d4e6522ec87ac0
ayemereodia2 <odiadavid2@gmail.com>
2022-09-01 13:25:37 +0100

    Added navigation to my startup namer app

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -14,14 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Start up Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Start up Name Generator'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white, foregroundColor: Colors.black)),
+      home: const RandomWords(),
     );
   }
 }
@@ -39,26 +35,72 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestion = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      final i = index ~/ 2;
+    return Scaffold(
+      appBar: AppBar(title: const Text(' Start up name Generator'), actions: [
+        IconButton(
+          onPressed: _pushSaved,
+          icon: const Icon(Icons.list),
+          tooltip: 'Saved suggestion',
+        )
+      ]),
+      body: ListView.builder(itemBuilder: (context, index) {
+        final i = index ~/ 2;
 
-      if (index.isOdd) {
-        return const Divider();
-      }
+        if (index.isOdd) {
+          return const Divider();
+        }
 
-      if (i >= _suggestion.length) {
-        _suggestion.addAll(generateWordPairs().take(10));
-      }
+        if (i >= _suggestion.length) {
+          _suggestion.addAll(generateWordPairs().take(10));
+        }
 
-      return ListTile(
-        title: Text(
-          _suggestion[i].asPascalCase,
-          style: _biggerFont,
+        final alreadySaved = _saved.contains(_suggestion[i]);
+
+        return ListTile(
+          title: Text(
+            _suggestion[i].asPascalCase,
+            style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Removed from savd' : 'Save',
+          ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestion[i]);
+              } else {
+                _saved.add(_suggestion[i]);
+              }
+            });
+          },
+        );
+      }),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tile = _saved.map((item) {
+        return ListTile(
+          title: Text(item.asPascalCase, style: _biggerFont),
+        );
+      });
+      final divided = tile.isNotEmpty
+          ? ListTile.divideTiles(tiles: tile, context: context).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved favourites'),
         ),
+        body: ListView(children: divided),
       );
-    });
+    }));
   }
 }
